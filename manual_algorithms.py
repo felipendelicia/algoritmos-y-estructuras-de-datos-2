01fb33cd048ca597d7e95243079156a49bd11fdb
felipendelicia <delicia4581@gmail.com>
2024-06-27 15:17:16 -0300

    feat: quick sort

--- manual_algorithms.py
+++ manual_algorithms.py
@@ -101,4 +101,22 @@ def merge_sort(arr: list[int])->list[int]:
 
     return merge(sorted_left_arr, sorted_right_arr)
 
-print(merge_sort(array))
+def partition(arr:list[int], low:int, high:int)->list[int]:
+    pivot = arr[high]
+    i = low - 1
+
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i = i + 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+
+    return i + 1
+
+def quick_sort(arr:list[int], low:int, high:int)->list[int]:
+    if low < high:
+        pi = partition(arr, low, high)
+
+        quick_sort(arr, low, pi - 1)
+        quick_sort(arr, pi + 1, high)
